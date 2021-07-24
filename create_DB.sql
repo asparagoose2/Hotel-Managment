9a9c6bc9bbfc94bed5d06f95ed0cb6a4122822d2
ofir <ofir.duchovne@gmail.com>
2021-07-24 11:52:42 +0300

    Base DB is working

--- create_DB.sql
+++ create_DB.sql
@@ -1,4 +1,5 @@
 CREATE database hotel_project;
+use hotel_project;
 
 create table room_type (
     room_type_id int AUTO_INCREMENT,
@@ -17,42 +18,82 @@ CREATE table address (
     PRIMARY KEY (address_id)
 );
 
+CREATE table building(
+    building_id int AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL,
+    capacity int NOT NULL,
+    PRIMARY KEY (building_id)
+);
+
  create table customer (
-     customer_id INT(8) AUTO_INCREMENT,
-     first_name VARCHAR(20) NO NULL,
-     last_name VARCHAR(20) NO NULL,
-     phone VARCHAR(10) NO NULL,
-     adderss_id INT(8),
-     FOREIGN KEY (adderss_id)
-     REFERENCES address (address_id)
-     PRIMARY KEY(customer_id)  
+    customer_id INT(8) AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    phone VARCHAR(10) NOT NULL,
+    adderss_id INT(8),
+    FOREIGN KEY (adderss_id)
+    REFERENCES address (address_id),
+    PRIMARY KEY(customer_id)  
      
- )
+);
 
-  create table room  (
-     room_number INT(8) AUTO_INCREMENT,
-     room_type VARCHAR(20) NO NULL,
-     building_id INT(8),
-     floor INT(8),
-     status VARCHAR(20),
-     PRIMARY KEY(room_number)     
- );
+create table room  (
+    room_number INT(8) AUTO_INCREMENT,
+    room_type VARCHAR(20) NOT NULL,
+    building_id INT(8),
+    floor INT(8),
+    status VARCHAR(20),
+    PRIMARY KEY(room_number)   
+);
 
-   create table employee  (
-     employee_id INT(8) AUTO_INCREMENT,
-     employee_type VARCHAR(20) NO NULL,
-     phone VARCHAR(11),
-     first_name VARCHAR(20),
-     last_name VARCHAR(20),
-     address_id VARCHAR(20),
-     FOREIGN KEY (employee_type)
-     REFERENCES address (address_id)
-     PRIMARY KEY(employee_ID)     
- );
+create table employee  (
+    employee_id INT(8),
+    employee_type VARCHAR(20) NOT NULL,
+    phone VARCHAR(11),
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    address_id INT,
+    FOREIGN KEY (address_id)
+    REFERENCES address (address_id),
+    PRIMARY KEY(employee_id)     
+);
+
+create table room_log ( 
+    room_number INT(8),
+    employee_id INT(8),
+    action VARCHAR(30) NOT NULL,
+    time TIMESTAMP DEFAULT (now()), 
+    PRIMARY KEY(room_number, employee_id),
+    FOREIGN KEY (room_number) REFERENCES room(room_number),
+    FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+    CONSTRAINT CHK_Action CHECK (action IN ("clean", "status change"))
+);
  
-    create table status_log (
-     room_number INT(8) AUTO_INCREMENT,
-     employee_id INT(8) AUTO_INCREMENT,
-     time TIMESTAMP DEFAULT (now()), 
-     PRIMARY KEY(room_number)     
+
+create table orders (
+    order_number INT AUTO_INCREMENT NOT NULL,
+    customer_id INT NOT NULL,
+    employee_id INT NOT NULL,
+    check_in TIMESTAMP,
+    check_out TIMESTAMP,
+    order_time TIMESTAMP DEFAULT (now()),
+    PRIMARY KEY(order_number),
+    FOREIGN KEY (employee_id)
+    REFERENCES employee (employee_id),
+    FOREIGN KEY (customer_id)
+    REFERENCES customer (customer_id)     
  );
+
+create table order_rooms (
+    employee_id INT NOT NULL,
+    order_number INT NOT NULL,
+    FOREIGN KEY (employee_id)
+    REFERENCES employee (employee_id),
+    FOREIGN KEY (order_number)
+    REFERENCES orders (order_number),
+    PRIMARY KEY(employee_id, order_number) 
+);
+
+
+
+ 
