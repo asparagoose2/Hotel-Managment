0995b57e15a2b21d0f48484663c337f549b7762e
ofir <ofir.duchovne@gmail.com>
2021-07-26 09:46:15 +0300

    ADDED add_rooms procedure

--- procedures.sql
+++ procedures.sql
@@ -13,3 +13,46 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+-- change room(s) status by order number
+DELIMITER $$
+CREATE PROCEDURE change_status(
+    IN in_order_number int,
+    IN in_emp_id INT,
+    IN new_status int
+)
+BEGIN
+    -- Check that new_status is valid
+    IF new_status NOT IN (SELECT room_status_id from room_status where room_status_id = new_status) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Illegal "New Status"';
+    END IF;
+    -- Updates the room(s) status
+    UPDATE room 
+    SET room_status = new_status 
+    where room_number in 
+    (select room_number from order_rooms where order_number = in_order_number);
+    -- Write in log the changes
+    INSERT INTO room_status_log (room_number, employee_id, new_status) 
+    select room_number,in_emp_id, new_status 
+    from order_rooms 
+    where order_number = in_order_number;
+end $$
+
+DELIMITER ;
+
+-- CALL change_status(10,100,1);
+
+
+-- Add random Rooms:
+DELIMITER $$
+CREATE PROCEDURE add_rooms(
+    IN rooms_count int
+)
+BEGIN
+    SET @my_counter = 0;
+ WHILE @my_counter < rooms_count DO
+    insert into room (room_type, building_id, floor, room_status) values (floor(rand()*4+1), floor(rand()*3+1), floor(rand()*5+1),floor(rand()*3+1));
+    SET @my_counter = @my_counter + 1;
+END WHILE;
+end $$
+DELIMITER ;
